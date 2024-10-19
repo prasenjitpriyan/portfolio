d22e190dbba1a9aafc8b7995d2a575fbd2bf734a
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-10-19 20:35:11 +0530

    Loading page added

---
+++ app/loading.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <main className="loader">
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        L
+      </span>
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        o
+      </span>
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        a
+      </span>
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        d
+      </span>
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        i
+      </span>
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        n
+      </span>
+      <span className="text-black opacity-0 animate-pass tracking-widest shadow-[2px_2px_3px_#919191]">
+        g
+      </span>
+      <span className="text-black opacity-0 animate-pass1 tracking-widest shadow-[2px_2px_3px_#919191]">
+        .
+      </span>
+      <span className="text-black opacity-0 animate-pass1 tracking-widest shadow-[2px_2px_3px_#919191]">
+        .
+      </span>
+    </main>
+  )
+}
+
+export default Loading
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'my-color-1': '#F26B5E',
+        'my-color-2': '#A64941',
+        'my-color-3': '#592723',
+        'my-color-4': '#F2F2F2',
+        'my-color-5': '#0D0D0D',
+        'my-color-6': '#A1A2A6',
+        'my-color-7': '#F2D0BD'
       },
-    },
+      animation: {
+        pass: '2s ease-in-out infinite',
+        pass1: '2s ease-in-out infinite'
+      },
+      keyframes: {
+        pass: {
+          '0%': {
+            opacity: '1'
+          },
+
+          '50%': {
+            opacity: '0'
+          },
+
+          '100%': {
+            opacity: '1'
+          }
+        },
+        pass1: {
+          '0%': {
+            opacity: '1'
+          },
+
+          '50%': {
+            opacity: '0'
+          },
+
+          '100%': {
+            opacity: '1'
+          }
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
