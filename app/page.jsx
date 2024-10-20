b689d26c683907d9549c47025273bf8dd1fb0b3e
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-10-20 10:21:10 +0530

    Background added to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
+'use client'
+
+import React from 'react'
+import { Boxes } from '@/components/ui/background-boxes'
+import { cn } from '@/lib/utils'
 import { NavbarDemo } from '@/components/Navbar'
+import BackgroundBoxesDemo from '@/components/BackgroundBoxesDemo'
 
 export default function Home() {
   return (
     <main>
-      <div className="relative h-full w-full bg-my-color-5">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#F26B5E2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:8px_8px]"></div>
-        <div className="min-h-screen w-screen">
-          <NavbarDemo />
-        </div>
-      </div>
+      <NavbarDemo />
+      <BackgroundBoxesDemo />
     </main>
   )
 }
