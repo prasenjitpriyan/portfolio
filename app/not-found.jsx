b686f77db0ae7a6c8dc0bd49290dd18ea540f05a
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-10-20 09:10:15 +0530

    Not Found page color modified

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,21 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 const NF = () => {
   return (
-    <div class="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
+    <div class="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0 bg-my-color-5">
       <div class="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
-        <p class="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-gray-300">
+        <p class="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-my-color-6">
           404
         </p>
-        <p class="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-300 mt-2">
+        <p class="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-my-color-6 mt-2">
           Page Not Found
         </p>
         <p class="text-lg md:text-xl lg:text-2xl text-gray-500 my-12">
           Sorry, the page you are looking for could not be found.
         </p>
-        <a
-          href="#"
-          class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
+        <Link
+          href={'/'}
+          class="flex items-center space-x-2 bg-my-color-1 hover:bg-my-color-2 text-gray-100 px-4 py-2 rounded transition duration-150"
           title="Return Home"
         >
           <svg
@@ -31,12 +32,12 @@ const NF = () => {
             ></path>
           </svg>
           <span>Return Home</span>
-        </a>
+        </Link>
       </div>
       <div class="w-1/2 lg:h-full flex lg:items-end justify-center p-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="w-full text-blue-600"
+          className="w-full text-my-color-1"
           data-name="Layer 1"
           viewBox="0 0 1120.592 777.916"
         >
