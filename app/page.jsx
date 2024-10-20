3480b3f2cf194a49d98536d81499b560b6d87fa7
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-10-20 22:39:10 +0530

    About page and featured projects page added

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,16 @@
 import React from 'react'
 import { NavbarDemo } from '@/components/Navbar'
 import BackgroundBoxesDemo from '@/components/BackgroundBoxesDemo'
+import AboutMe from '@/components/AboutMe'
+import FeaturedProjects from '@/components/FeaturedProjects'
 
 export default function Home() {
   return (
     <main>
       <NavbarDemo />
       <BackgroundBoxesDemo />
+      <AboutMe />
+      <FeaturedProjects />
     </main>
   )
 }
---
+++ components/FeaturedProjects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeaturedProjects = () => {
+  return (
+    <div className="h-screen">
+      <div class="relative h-full w-full bg-my-color-1">
+        <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#0D0D0D2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:8px_8px]"></div>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedProjects
