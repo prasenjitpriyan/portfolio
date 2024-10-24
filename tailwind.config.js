850defaf067ce83b4cdcb5b1e1a7c59a63a0917b
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-10-24 20:01:16 +0530

    Color added to the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      'turquoise-blue': {
+        50: '#eaffff',
+        100: '#cbfeff',
+        200: '#9efaff',
+        300: '#5bf3ff',
+        400: '#00e1ff',
+        500: '#00c5e5',
+        600: '#009dc0',
+        700: '#037c9b',
+        800: '#0d647d',
+        900: '#105269',
+        950: '#033649'
       },
-    },
+      'chartreuse-yellow': {
+        50: '#ffffe4',
+        100: '#fdffc4',
+        200: '#faff90',
+        300: '#f1ff50',
+        400: '#e1ff00',
+        500: '#c5e600',
+        600: '#99b800',
+        700: '#738b00',
+        800: '#5b6d07',
+        900: '#4c5c0b',
+        950: '#283400'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
