60cfa68af0ee8d057fe3ad37efd102cc0a0961b7
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-10-27 21:31:54 +0530

    Project clean up

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import localFont from 'next/font/local'
+import './globals.css'
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900'
+})
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900'
+})
+
+export const metadata = {
+  title:
+    'Dynamic Solutions: JavaScript & TypeScript Development with Prasenjit Das',
+  description:
+    'Welcome to my portfolio! I’m a dedicated JavaScript and TypeScript developer passionate about building robust and scalable web applications. Here, you will find a collection of my projects that highlight my expertise in crafting seamless user experiences, optimizing performance, and implementing best practices. Dive into my work to see how I transform ideas into dynamic solutions, leveraging the power of modern frameworks and libraries.'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    colors: {
+      'my-color-1': '#222831',
+      'my-color-2': '#323330',
+      'my-color-3': '#61dbfb',
+      'my-color-4': '#ebebeb',
+      'my-color-5': '#f0db4f',
+      'my-color-6': '#f06529',
+      'my-color-7': '#2965f1',
+      'my-color-8': '#007acc',
+      'my-color-8': '#764abc'
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
