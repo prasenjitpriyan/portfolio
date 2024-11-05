191090e82197aa192f7c75ec8f9c11b8c48013b0
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-11-05 21:14:39 +0530

    CONTACT PAGE MODIFIED

--- components/ImageComponent.jsx
+++ components/ImageComponent.jsx
@@ -1,11 +1,10 @@
 'use client'
 
-// ImageComponent.js
 import React from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 
-const MotionImage = motion(Image) // Create a motion-enhanced version of Image
+const MotionImage = motion(Image)
 
 const ImageComponent = ({ src, alt }) => {
   return (
@@ -16,12 +15,12 @@ const ImageComponent = ({ src, alt }) => {
       alt={alt}
       className="w-full max-w-md rounded-lg shadow-lg"
       whileHover={{
-        scale: 1.1, // Scale the image slightly on hover
-        rotate: 2, // Slight rotation for a dynamic effect
-        border: '2px solid #ebebeb', // Change border on hover
-        boxShadow: '0 10px 20px rgba(0, 0, 0, 0.3)' // Add a shadow effect
+        scale: 1.1,
+        rotate: 2,
+        border: '2px solid #ebebeb',
+        boxShadow: '0 10px 20px rgba(0, 0, 0, 0.3)'
       }}
-      transition={{ duration: 0.3 }} // Smooth transition for the effects
+      transition={{ duration: 0.3 }}
     />
   )
 }
--- pages/ContactPage.jsx
+++ pages/ContactPage.jsx
@@ -1,53 +1,62 @@
 import React from 'react'
+import ImageComponent from '@/components/ImageComponent'
 
 const ContactPage = () => {
   return (
     <section className="h-auto bg-my-color-2 max-w-8xl p-8">
-      <div className="mx-auto bg-my-color-4 rounded-lg shadow-lg p-12">
-        <h1 className="text-3xl font-bold text-my-color-2 text-center mb-6">
-          Contact Me
-        </h1>
-        <form>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-my-color-2">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              required
-              className="w-full p-3 border border-my-color-2 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              required
-              className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block text-gray-700">
-              Message
-            </label>
-            <textarea
-              id="message"
-              required
-              rows="4"
-              className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white font-semibold py-3 rounded-md hover:bg-blue-700 transition duration-200"
-          >
-            Submit
-          </button>
-        </form>
+      <div className="mx-auto rounded-lg shadow-lg p-6 md:flex md:p-12">
+        {/* Left Column: Image */}
+        <div className="flex-shrink-0 mb-6 md:mb-0 md:w-1/2 md:pr-6">
+          <ImageComponent src="/myIMG.jpg" alt="Description of the image" />
+        </div>
+
+        {/* Right Column: Contact Form */}
+        <div className="md:w-1/2">
+          <h1 className="text-3xl font-bold text-my-color-4 text-center md:text-left mb-6">
+            Contact Me
+          </h1>
+          <form>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-my-color-4">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                required
+                className="w-full p-3 border border-my-color-4 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-my-color-4">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                required
+                className="w-full p-3 border border-my-color-4 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="message" className="block text-my-color-4">
+                Message
+              </label>
+              <textarea
+                id="message"
+                required
+                rows="4"
+                className="w-full p-3 border border-my-color-4 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-my-color-4 text-white font-semibold py-3 rounded-md hover:bg-my-color-5 transition duration-200"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   )
