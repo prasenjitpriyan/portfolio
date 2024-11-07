ba5910b4aa148ab7c9fff5025f286a8babe64cc9
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-11-07 10:42:18 +0530

    User route modified and blog route created with model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcryptjs'
 
 const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
-      unique: true
+      required: true
     },
     email: {
       type: String,
@@ -16,25 +14,15 @@ const UserSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true
+    },
+    profileImage: {
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
     }
   },
   { timestamps: true }
 )
-
-// Encrypt the password before saving the user
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
-
-  // Generate salt for hashing password
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-  next()
-})
-
-// Method to compare password during login
-UserSchema.methods.comparePassword = async function (password) {
-  return bcrypt.compare(password, this.password)
-}
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema)
 
