1b180090220c5ff0107e858702b988003abfc7c2
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-12-29 10:02:06 +0530

    Sidebar modified

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 import SocialLinks from './SocialLinks'
 import NavLinks from './NavLinks'
 import MotionWrapper from '@/components/MotionWrapper'
-import { useSidebar } from '@/context/SidebarContext' // Import the custom hook
+import { useSidebar } from '@/context/SidebarContext'
 import Link from 'next/link'
 
 const Sidebar = () => {
-  const { isOpen } = useSidebar() // Consume state
+  const { isOpen } = useSidebar()
 
   return (
     <div
@@ -18,7 +18,7 @@ const Sidebar = () => {
     >
       <div className="flex flex-col items-center">
         {/* Profile Image */}
-        <div className="mb-4 bg-my-color-2 p-2 rounded-full border-8 border-my-color-3 shadow-lg">
+        <div className="mb-4 bg-my-color-2 p-2 rounded-full border-8 border-my-color-4 hover:border-my-color-3 shadow-lg">
           <MotionWrapper
             initial={{ opacity: 0, scale: 0.8, rotate: -10 }}
             animate={{ opacity: 1, scale: 1, rotate: 0 }}
@@ -29,8 +29,8 @@ const Sidebar = () => {
               stiffness: 100
             }}
             whileHover={{
-              scale: 1.1, // Slight zoom effect
-              rotate: 5, // Subtle rotation
+              scale: 1.1,
+              rotate: 5,
               transition: { type: 'spring', stiffness: 150, damping: 10 }
             }}
           >
@@ -53,15 +53,11 @@ const Sidebar = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, ease: 'easeOut', delay: 0.2 }}
         >
-          <h1 className="text-2xl text-my-color-3 font-bold mb-6">
+          <h1 className="text-2xl text-my-color-4 font-bold mb-6">
             Prasenjit Das
           </h1>
         </MotionWrapper>
-
-        {/* Social Media Icons */}
         <SocialLinks />
-
-        {/* Navigation Links */}
         <NavLinks />
       </div>
     </div>
