bd171f7f3f7d95698d62347fed67a68c0ab26ea7
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-12 13:10:10 +0530

    Image deputed to project section

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import Button from './Button'
 import { GoArrowRight } from 'react-icons/go'
 
@@ -9,7 +10,7 @@ const projects = [
     title: 'Project One',
     category: 'WEB APP',
     link: '/projects/salinaka',
-    image: '/images/salinaka-showcase.jpg'
+    image: '/my.jpg'
   },
   {
     id: 2,
@@ -17,7 +18,7 @@ const projects = [
     title: 'Project Two',
     category: 'WEB APP',
     link: '/projects/project-two',
-    image: '/images/project-two-showcase.jpg'
+    image: '/my.jpg'
   },
   {
     id: 3,
@@ -25,7 +26,7 @@ const projects = [
     title: 'Project Three',
     category: 'MOBILE APP',
     link: '/projects/project-three',
-    image: '/images/project-three-showcase.jpg'
+    image: '/my.jpg'
   },
   {
     id: 4,
@@ -33,7 +34,7 @@ const projects = [
     title: 'Project Four',
     category: 'DESIGN SYSTEM',
     link: '/projects/project-four',
-    image: '/images/project-four-showcase.jpg'
+    image: '/my.jpg'
   }
 ]
 
@@ -43,16 +44,27 @@ const ProjectSection: React.FC = () => {
       {projects.map((project) => (
         <div
           key={project.id}
-          className="h-1/4 border-t-2 hover:bg-white hover:text-black transition-all duration-300 ease-in-out"
+          className="relative h-1/4 border-t-2 hover:bg-white hover:text-black transition-all duration-300 ease-in-out group"
         >
+          {/* Background Image for Hover */}
+          <div className="absolute inset-y-0 left-0 w-full group-hover:translate-x-0 translate-x-[-100%] transition-all duration-500 ease-in-out">
+            <div className="relative w-full h-full opacity-25 group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
+              <Image
+                width={300}
+                height={600}
+                src={project.image}
+                alt={project.title}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+
           <div
             className="flex flex-col sm:flex-row items-center justify-between py-10"
             style={{ transform: 'translate(0px, 0px)', opacity: 1 }}
           >
             {/* Left side: Line and project details */}
             <div className="flex items-center mb-4 sm:mb-0">
-              {' '}
-              {/* Added margin-bottom for small screens */}
               <div className="h-[1px] w-40 bg-gray-300 mr-4" />
               <p className="font-bold text-xl mr-4 text-gray-300">
                 {project.number}
