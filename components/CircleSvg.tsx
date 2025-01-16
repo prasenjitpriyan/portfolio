5ce6dd5fffe651f462b05fddfb45121a0dcfbcc5
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-16 08:10:23 +0530

    motion added and apply to circle svg

--- components/CircleSvg.tsx
+++ components/CircleSvg.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from 'react'
+import { motion } from 'framer-motion'
 
 interface CircleSvgProps {
   className?: string
@@ -9,22 +12,21 @@ const CircleSvg: React.FC<CircleSvgProps> = ({ className }) => {
     <div
       className={`absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex justify-center ${className}`}
     >
-      <svg
+      <motion.svg
         width="500"
         height="500"
         viewBox="0 0 200 200"
         xmlns="http://www.w3.org/2000/svg"
         className="bg-white rounded-full"
+        initial={{ scale: 0, opacity: 0, rotate: -90 }}
+        animate={{ scale: 1, opacity: 1, rotate: 0 }}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut'
+        }}
       >
-        <circle
-          cx="100"
-          cy="100"
-          r="90"
-          fill="none"
-          stroke="jet-black"
-          strokeWidth="1"
-        />
-      </svg>
+        <circle cx="100" cy="100" r="90" fill="none" />
+      </motion.svg>
     </div>
   )
 }
