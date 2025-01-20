7816985c4e24e76564df55e550b191f6a3c04843
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-20 08:40:20 +0530

    Motion added to the footer component and MotionDiv modified for reuse

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import Link from 'next/link'
 import { GoArrowRight } from 'react-icons/go'
 import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 import Round from './Round'
 import Circle from './Circle'
 import Button from './Button'
+import MotionDiv from './MotionDiv'
+
+type SocialLink = {
+  href: string
+  icon: JSX.Element
+}
+
+const socialLinks: SocialLink[] = [
+  { href: 'https://facebook.com', icon: <FaFacebook /> },
+  { href: 'https://twitter.com', icon: <FaTwitter /> },
+  { href: 'https://linkedin.com', icon: <FaLinkedin /> },
+  { href: 'https://github.com', icon: <FaGithub /> }
+]
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear()
@@ -12,14 +25,19 @@ const Footer: React.FC = () => {
   return (
     <section className="h-screen relative bg-black text-white flex flex-col justify-between">
       <Round className="absolute top-0 right-80" />
-      {/* Top Section */}
-      <div className="flex justify-center items-center pt-28">
-        <h1 className="text-5xl font-bold">Let&apos;s work together.</h1>
-      </div>
 
-      {/* Middle Section (Centered Vertically) */}
-      <div className="flex flex-col justify-center items-center flex-grow gap-4">
-        <h3 className="text-lg font-semibold p-4 text-gray-400">
+      {/* Top Section */}
+      <MotionDiv className="flex justify-center items-center pt-28">
+        <h1 className="text-5xl font-bold">Let&apos;s work together.</h1>
+      </MotionDiv>
+
+      {/* Middle Section */}
+      <MotionDiv
+        className="flex flex-col justify-center items-center flex-grow gap-4"
+        delay={0.3}
+        animationType="fadeInScale"
+      >
+        <h3 className="text-xl p-4 text-gray-200 font-thin">
           I&apos;m available for freelance work.
         </h3>
         <Button
@@ -27,31 +45,36 @@ const Footer: React.FC = () => {
           href="/contact"
           icon={<GoArrowRight className="text-2xl" />}
           className="text-white"
-          circleColor="bg-gray-500"
+          circleColor="bg-jet-black"
           hoverWidth="140px"
         />
-      </div>
+      </MotionDiv>
 
       <Circle className="absolute overflow-hidden w-48 h-48 left-[-70px] bottom-[-70px]" />
+
       {/* Bottom Section */}
-      <footer className="w-full flex justify-center items-center p-12 gap-8 text-xs">
+      <MotionDiv
+        className="w-full flex justify-center items-center p-12 gap-8 text-xs"
+        delay={0.6}
+        animationType="fadeInUp"
+      >
         <p className=" text-white">© {currentYear} Prasenjit Das</p>
         <p className="text-xl">||</p>
         <div className="flex gap-2">
-          <Link href="https://facebook.com" target="_blank" rel="noreferrer">
-            <FaFacebook className="" />
-          </Link>
-          <Link href="https://twitter.com" target="_blank" rel="noreferrer">
-            <FaTwitter className="" />
-          </Link>
-          <Link href="https://linkedin.com" target="_blank" rel="noreferrer">
-            <FaLinkedin className="" />
-          </Link>
-          <Link href="https://github.com" target="_blank" rel="noreferrer">
-            <FaGithub className="" />
-          </Link>
+          {socialLinks.map((link, index) => (
+            <MotionDiv key={index} animationType="fadeInScale" delay={0.9}>
+              <Link
+                href={link.href}
+                target="_blank"
+                rel="noreferrer"
+                className="h-8 w-8 flex justify-center items-center bg-black rounded-full hover:bg-white hover:text-black"
+              >
+                {link.icon}
+              </Link>
+            </MotionDiv>
+          ))}
         </div>
-      </footer>
+      </MotionDiv>
     </section>
   )
 }
--- components/MotionDiv.tsx
+++ components/MotionDiv.tsx
@@ -1,20 +1,43 @@
 'use client'
 
-import React from 'react'
-import { motion, MotionProps } from 'framer-motion'
+import { motion } from 'framer-motion'
 
-interface MotionDivProps extends MotionProps {
+type MotionDivProps = {
   children: React.ReactNode
   className?: string
+  delay?: number
+  animationType?: 'fadeInUp' | 'fadeInScale' | 'fadeIn'
 }
 
 const MotionDiv: React.FC<MotionDivProps> = ({
   children,
   className,
-  ...motionProps
+  delay = 0,
+  animationType = 'fadeInUp'
 }) => {
+  const variants = {
+    fadeInUp: {
+      initial: { opacity: 0, y: 50 },
+      whileInView: { opacity: 1, y: 0 }
+    },
+    fadeInScale: {
+      initial: { opacity: 0, scale: 0.8 },
+      whileInView: { opacity: 1, scale: 1 }
+    },
+    fadeIn: {
+      initial: { opacity: 0 },
+      whileInView: { opacity: 1 }
+    }
+  }
+
   return (
-    <motion.div {...motionProps} className={className}>
+    <motion.div
+      initial={variants[animationType].initial}
+      whileInView={variants[animationType].whileInView}
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{ delay, duration: 1 }}
+      className={className}
+    >
       {children}
     </motion.div>
   )
