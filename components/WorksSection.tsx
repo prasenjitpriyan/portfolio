751cd57f9c0e35d6b08d833b7c7841682c59bd7a
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-21 19:12:03 +0530

    Work section modified with motion

--- components/WorksSection.tsx
+++ components/WorksSection.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 import Header from './Header'
 
 const WorksSection: React.FC = () => {
@@ -22,17 +23,31 @@ const WorksSection: React.FC = () => {
         <p className="text-xs">WORKS</p>
       </div>
       <div className="flex justify-center items-center border-2 h-screen relative">
-        <Image
-          src={'/work.png'}
-          alt="work"
-          width={500}
-          height={500}
-          className="px-12 md:px-0 opacity-80 md:h-[500px] md:w-[500px]"
-          priority
-        />
-        <h1 className="absolute text-6xl md:text-7xl text-black font-bold">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8, rotate: -90 }}
+          whileInView={{ opacity: 1, scale: 1, rotate: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+        >
+          <Image
+            src={'/work.png'}
+            alt="work"
+            width={500}
+            height={500}
+            className="px-12 md:px-0 opacity-80 md:h-[500px] md:w-[500px]"
+            priority
+          />
+        </motion.div>
+
+        <motion.h1
+          className="absolute text-6xl md:text-7xl text-black font-bold"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.8, ease: 'easeOut', delay: 0.3 }}
+        >
           MY WORKS
-        </h1>
+        </motion.h1>
         {/* Animated Arrow */}
         <div className="absolute bottom-8 flex flex-col items-center">
           <svg
