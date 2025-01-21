0f62fe1687ef775dc2790fc898cde4d3d917a598
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-01-21 20:22:18 +0530

    Works project modified

--- components/WorksProject.tsx
+++ components/WorksProject.tsx
@@ -47,7 +47,7 @@ const WorksProject: React.FC = () => {
                 text="VIEW PROJECT"
                 href={`/works/${project.slug}`}
                 icon={<GoArrowRight className="text-2xl" />}
-                className="relative inline-flex items-center transition-all duration-300 ease-in-out"
+                className="relative text-black inline-flex items-center transition-all duration-300 ease-in-out"
               />
             </div>
           </div>
