40f667feb03911a5b5f16777c415285fdef422e7
Prasenjit Das <prasenjit@Prasenjits-MacBook-Air.local>
2025-01-30 22:00:12 +0530

    Studio combine to a group folder

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export const metadata: Metadata = {
+  title: 'Prasenjit Das || Sanity',
+  description:
+    'From delivering postal services to delivering code – Crafting solutions with JavaScript & TypeScript.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${roboto.variable} antialiased scrollbar`}>
+        {children}
+      </body>
+    </html>
+  );
+}
