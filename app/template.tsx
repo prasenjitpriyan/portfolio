5df9f616888ecdfdf4c545208077437794ddea09
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-08-08 19:42:13 +0530

    GSAP added and page template modified

--- app/template.tsx
+++ app/template.tsx
@@ -1,15 +1,31 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { animatePageIn } from '@/lib/animations'
+import { useEffect } from 'react'
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    animatePageIn()
+  }, [])
   return (
-    <motion.div
-      initial={{ y: 20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 0.75 }}
-    >
+    <div>
+      <div
+        id="banner-1"
+        className="min-h-screen bg-black z-10 fixed top-0 left-0 w-1/4"
+      />
+      <div
+        id="banner-2"
+        className="min-h-screen bg-black z-10 fixed top-0 left-1/4 w-1/4"
+      />
+      <div
+        id="banner-3"
+        className="min-h-screen bg-black z-10 fixed top-0 left-2/4 w-1/4"
+      />
+      <div
+        id="banner-4"
+        className="min-h-screen bg-black z-10 fixed top-0 left-3/4 w-1/4"
+      />
       {children}
-    </motion.div>
+    </div>
   )
 }
