1cef06d0f6ff4a6a9d82dcc440f156dc919caa42
prasenjitpriyan <prasenjitpriyan@gmail.com>
2025-10-11 11:48:30 +0530

    Not found page animation added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,49 +1,46 @@
-'use client'
+'use client';
 
-import Header from '@/components/Header'
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
+import Error404 from '@/components/Error404';
+import Header from '@/components/Header';
+import { AnimatePresence } from 'framer-motion';
+import Link from 'next/link';
+import React, { useState } from 'react';
 
 const NotFound: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen((prev) => !prev)
-  }
+    setIsMenuOpen((prev) => !prev);
+  };
   return (
-    <section className="relative h-screen bg-ghost-white text-jet-black flex flex-col">
-      {/* Header */}
-      <Header toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
-      <div className="relative flex flex-1 items-center px-20 py-20 md:flex-row flex-col-reverse z-20">
-        <div className="flex-1 space-y-6 text-center">
-          <h1 className="text-3xl md:text-7xl font-semibold">404</h1>
-          <p className="text-2xl font-bold">Page not found</p>
-          <p className="text-xl pb-4">
-            The page you are looking for doesn&apos;t exist.
-          </p>
-          <button>
-            <Link
-              href={'/'}
-              className="bg-jet-black text-white px-8 py-4 rounded"
-            >
-              Go Back
-            </Link>
-          </button>
+    <AnimatePresence mode="wait">
+      <section className="relative h-screen bg-ghost-white text-jet-black flex flex-col">
+        {/* Header */}
+        <Header toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} />
+        <div className="relative flex flex-1 items-center px-20 py-20 md:flex-row flex-col-reverse z-20">
+          <div className="flex-1 space-y-6 text-center">
+            <h1 className="text-3xl md:text-7xl font-semibold">404</h1>
+            <p className="text-2xl font-bold">Page not found</p>
+            <p className="text-xl pb-4">
+              The page you are looking for doesn&apos;t exist.
+            </p>
+            <button>
+              <Link
+                href={'/'}
+                className="bg-jet-black text-white px-8 py-4 rounded">
+                Go Back
+              </Link>
+            </button>
+          </div>
+          <div className="flex-1 flex justify-center items-center relative z-20 h-96 md:h-full">
+            <div className="w:[250] h-[250] sm:w-[300] sm:h-[300] md:w-[400] md:h-[400] lg:w-[500] lg:h-[500] flex justify-center">
+              <Error404 />
+            </div>
+          </div>
         </div>
-        <div className="flex-1 flex justify-center items-center relative z-20 h-96 md:h-full">
-          <Image
-            width={500}
-            height={500}
-            src="/404.svg"
-            alt="Prasenjit Das"
-            priority={true}
-            className="w-full h-full"
-          />
-        </div>
-      </div>
-    </section>
-  )
-}
+      </section>
+    </AnimatePresence>
+  );
+};
 
-export default NotFound
+export default NotFound;
