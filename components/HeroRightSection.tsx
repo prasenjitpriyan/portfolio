a985f1dee7b131a6e1e4d1cc4bbe82b2a95a4b6c
prasenjitpriyan <prasenjitpriyan@gmail.com>
2026-07-13 19:58:40 +0530

    Fix hero section image responsiveness and perfectly center animated images

--- components/HeroRightSection.tsx
+++ components/HeroRightSection.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const HeroRightSection: React.FC = () => {
   return (
-    <div className="flex-1 flex justify-center items-center relative z-20 h-96 md:h-full">
+    <div className="flex-1 relative h-96 md:h-full">
       {/* First Image with circulating animation */}
-      <div className="w-full h-full animate-rotate-once z-0">
+      <div className="absolute top-1/2 left-1/2 animate-rotate-once flex justify-center items-center w-62.5 h-62.5 md:w-155 md:h-155">
         <Image
           width={420}
           height={420}
@@ -17,14 +17,16 @@ const HeroRightSection: React.FC = () => {
       </div>
 
       {/* Second Image on top, centered with bounce effect */}
-      <Image
-        width={400}
-        height={400}
-        src="/3.png"
-        alt="Additional Image"
-        priority={true}
-        className="absolute z-10 rounded-full top-1/2 left-1/2 w-50 h-50 md:w-100 md:h-100 animate-bounce-in-place"
-      />
+      <div className="absolute top-1/2 left-1/2 animate-bounce-in-place pointer-events-none flex justify-center items-center w-50 h-50 md:w-100 md:h-100">
+        <Image
+          width={400}
+          height={400}
+          src="/3.png"
+          alt="Additional Image"
+          priority={true}
+          className="w-full h-full rounded-full object-cover pointer-events-auto"
+        />
+      </div>
     </div>
   )
 }
